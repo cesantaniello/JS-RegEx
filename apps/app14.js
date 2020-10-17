0056c6190ee1583e3246a36231d2e217686d946c
Carlos <carlos.e.santaniello@gmail.com>
2020-10-18 00:10:18 +0200

    Actualizacion Metodo Replace

--- apps/app14.js
+++ apps/app14.js
@@ -1,3 +1,5 @@
+//Método Replace
+
 var mensaje = "El cliente pidió arepa y se comió su arepa";
 
-console.log(mensaje.replace(/arepa/g, 'empanada'));
+console.log(mensaje.replace(/arepa/g, 'empanada')); //g busca el término a nivel global
