0699e4a932daeff55ad6ea39f41c3553068c8f54
Carlos <carlos.e.santaniello@gmail.com>
2020-10-18 00:30:54 +0200

    4.3 Propiedad LastIndex

---
+++ apps/app16.js
@@ -0,0 +1,6 @@
+//Propiedad Last Index
+
+var mensaje = /\d/g; //Indica la posición del último número del mensaje
+
+console.log(mensaje.exec('Pedí 4 buñuelos y 2 chocolates'));
+console.log(mensaje.exec('Pedí 4 buñuelos y 2 chocolates'));
